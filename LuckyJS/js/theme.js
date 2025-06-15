373275cedd97f91302876afcedfced2f885debe4
K1llua66 <shitosasageo@gmail.com>
2025-06-15 23:47:08 +0300

    Add files via upload

---
+++ LuckyJS/js/theme.js
@@ -0,0 +1,16 @@
+let body = document.querySelector("body")
+
+
+
+
+function themeDark(){
+    body.style.background = "#191970"
+    body.style.color = "#FFFFFF"
+}
+
+function themeLight(){
+    body.style.background = "#FFFFFF"
+    body.style.color = "#000000"
+}
+
+
